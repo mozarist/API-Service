86ac500902ca4e2f77af230b950a77f2e8510d90
mozarist <azzammozarist.xpro@gmail.com>
2026-04-20 14:49:12 +0700

    Add Product model and migration with fillable attributes

--- database/migrations/2026_04_20_074240_create_products_table.php
+++ database/migrations/2026_04_20_074240_create_products_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('sku')->unique();
+            $table->string('name');
+            $table->string('brand');
+            $table->integer('price');
+            $table->integer('stock')->default(0);
+            $table->string('category');
             $table->timestamps();
         });
     }
